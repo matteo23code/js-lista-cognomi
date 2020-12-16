e4a62f0134aab6a8558a77aa93a762be6e9c7b2a
Matteo Basile <matteo.basile56@gmail.com>
2020-12-16 16:45:57 +0100

    es completo

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 var domanda = prompt('Qual è il suo cognome?');
 
 var listaOutput = document.getElementById('lista-cognomi');
-var listaCognomi = ['basile', 'rossi', 'gennari', 'leoni', 'cavola', 'rinaldi', 'neri']
+var listaCognomi = ['basile', 'rossi', 'gennari', 'leoni', 'cavola', 'rinaldi', 'neri'];
 listaCognomi.push(domanda);
-var contenutoPrecedente;
+listaCognomi.sort();
+
+var contenutoPrecedente = domanda + listaCognomi;
 
 for (var i = 0; i < listaCognomi.length; i++) {
-     console.log(listaCognomi[i]);
+  console.log(listaCognomi[i]);
   contenutoPrecedente = listaOutput.innerHTML;
   listaOutput.innerHTML = contenutoPrecedente + '<li>' + listaCognomi[i] + '</li>';
-  console.log(listaCognomi.indexOf(domanda 1));
+  console.log(listaCognomi.indexOf(domanda));
 }
